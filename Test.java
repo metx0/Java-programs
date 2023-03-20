f708767ff6a8a10e2c337ef1b74f025872ef5719
metx0 <119399016+metx0@users.noreply.github.com>
2023-03-19 23:13:20 -0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Test {
 	public static void main(String[] args) {
-
+		
+		/*
 		// Para probar la clase Ejecutor:
 		
 		Ejecutor e = new Ejecutor("calc", 1);
 
-		/* Scanner leer = new Scanner(System.in);
+		Scanner leer = new Scanner(System.in);
 		System.out.println("Escribe un comando para ejecutar");
-		e.setComando(leer.next()); */
+		e.setComando(leer.next()); 
 
 		// e.ejecutarTarea();
 
@@ -19,6 +20,9 @@ public class Test {
 
 		if (r.necesitaEjecucion()) {
 			r.ejecutarTarea();
-		}
+		} */
+		
+		new Thread(new Reloj()).start();
+		new Thread(new Ejecutor("calc", 0)).start();
 	}
 }
